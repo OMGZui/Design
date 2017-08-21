2c81d6be31d2094b5aa74319b8ee9ec63c2778b0
shengj <shengj@fangde.com>
2017-08-21 22:46:02 +0800

    MotherObserver

--- book/Observer/index.php
+++ book/Observer/index.php
@@ -14,11 +14,13 @@ echo "-------------------------  观察者模式  -------------------------\n";
 //这里秋水深深的喜欢上了肖红，真是造孽啊，我本人是挺喜欢赵英男的，可惜啊。而且还开启了单例模式。
 //秋水选择肖红，发布消息
 $observer = new Subject('XH');
-//同时通知赵英男和肖红两个观察者
+//同时通知赵英男和肖红两个观察者加上老妈
 $observer->attach(ZYNObserver::getInstance());
 $observer->attach(XHObserver::getInstance());
 $observer->detach(XHObserver::getInstance());
+$observer->attach(MotherObserver::getInstance());
 //通知
 $observer->notify();
 //赵英男收到后显示 "我选择了肖红"
-//肖红收到后显示 "我选择了你 -> 肖红 "
+//肖红收到后显示 "我选择了你 -> 肖红"
+//老妈收到后显示 "我选择了她 -> 肖红"
